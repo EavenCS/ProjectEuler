8aa2c576e8829702949ca162825815667e3275c5
EavenCS <eaven7juni@gmail.com>
2024-09-22 16:36:08 +0200

    Add project files.

---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectEuler;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Select your Problem in the Debugger.\nPress any key to cancel the application");
+        Console.ReadKey();
+    }
+}
