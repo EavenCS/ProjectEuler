856131d3060b728c6a27fecfa5386f5ae972f18b
EavenCS <eaven7juni@gmail.com>
2024-09-22 21:17:42 +0200

    feat(Problem_0003): Third problem solved

---
+++ Problem_0003/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Problem_0003;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        long number = 600851475143;
+        long largestPrimeFactor = FindLargestPrimeFactor(number);
+        Console.WriteLine($"The largest prime factor of {number} is {largestPrimeFactor}");
+    }
+
+    static long FindLargestPrimeFactor(long n)
+    {
+        long largestFactor = 1;
+
+        while (n % 2 == 0)
+        {
+            largestFactor = 2;
+            n /= 2;
+        }
+
+        for (long i = 3; i * i <= n; i += 2)
+        {
+            while (n % i == 0)
+            {
+                largestFactor = i;
+                n /= i;
+            }
+        }
+
+        if (n > 2)
+        {
+            largestFactor = n;
+        }
+
+        return largestFactor;
+    }
+}
